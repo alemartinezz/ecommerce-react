eda29e218384ad22a3967d82179c1e726ffa45fe
Ale <alemartinezz13@protonmail.com>
2022-02-02 19:19:39 -0300

    navbar tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Title from './components/Title/Title'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Alejandro Martínez: Backend Developer aprendiendo ReactJS !
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Title />
     </div>
   );
 }
---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import './title.css'
+
+function Title() {
+    return (
+        <section>
+            <h1>Clase 2 : Crear Navbar</h1>
+            <h3>Framework: Tailwind CSS</h3>
+        </section>
+    )
+}
+
+export default Title
